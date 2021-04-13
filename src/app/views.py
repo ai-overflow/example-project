061ebdd3b5cf817f78e31a8b326554ed300097af
Sebastian <19Sebastian95@gmx.de>
2021-04-13 16:19:02 +0200

    added json schema

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,12 @@
 from app import app
+from flask import request
+import json
 
 @app.route('/')
 def home():
    return "hello world!"
+
+
+@app.route('/image/test/<algo>/', methods=['POST'])
+def algo_test(algo):
+   return "You have requested this page and expected {algo:}\nHeaders: {headers:}".format(algo = algo, headers = json.dumps(dict(request.headers)))

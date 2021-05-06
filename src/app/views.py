1fa92fa973b3266715d94013fae76eabc5ce7693
Sebastian <19Sebastian95@gmx.de>
2021-05-07 01:12:37 +0200

    update example application

--- src/app/views.py
+++ src/app/views.py
@@ -1,12 +1,21 @@
-from app import app
-from flask import request
 import json
+from flask import request, abort
+from app import app
+
 
 @app.route('/')
 def home():
-   return "hello world!"
+    abort(403)
 
 
-@app.route('/image/test/<algo>/', methods=['POST'])
+@app.route('/process/algo/<algo>/', methods=['GET'])
 def algo_test(algo):
-   return "You have requested this page and expected {algo:}\nHeaders: {headers:}".format(algo = algo, headers = json.dumps(dict(request.headers)))
+    if request.headers["Content-Type"] not in ["image/jpg", "image/png", "image/gif"]:
+        abort(400)
+    if algo not in ["Fast", "Accurate"]:
+        abort(400)
+
+    data = request.get_data()
+    return "You have requested this page and expected {algo:}\nHeaders: {headers:}".format(algo=algo,
+                                                                                           headers=json.dumps(
+                                                                                               dict(request.headers)))
